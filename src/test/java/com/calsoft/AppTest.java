a388c020beeca565fa970789d0a2c943a9aa498e
sandhyarao17 <90891350+sandhyarao17@users.noreply.github.com>
2023-01-02 23:53:36 +0530

    Update AppTest.java

--- src/test/java/com/calsoft/AppTest.java
+++ src/test/java/com/calsoft/AppTest.java
@@ -1,39 +1,28 @@
 package com.calsoft;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+import java.io.*;
+import org.junit.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
     public void testApp()
     {
         assertTrue( true );
     }
+
+    @Test
+public void testmain() {
+    PrintStream originalOut = System.out;
+    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(bos));
+    App.main(null);
+    System.out.println("#"+bos.toString()+"#");
+    //Assert.assertTrue("Hello-World".equals(bos.toString()));
+    System.setOut(originalOut);
+        }
+
 }
-//Just to show git demo
