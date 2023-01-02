ddfab6e5704cf303e3d24c7b2e15779e4a102dd8
root <root@ip-172-31-41-155.ap-south-1.compute.internal>
2023-01-02 17:31:48 +0000

    added changes

--- src/test/java/com/calsoft/AppTest.java
+++ src/test/java/com/calsoft/AppTest.java
@@ -1,8 +1,11 @@
 package com.calsoft;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+//import junit.framework.Test;
+//import junit.framework.TestCase;
+//import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+import java.io.*;
+import org.junit.*;
 
 /**
  * Unit test for simple App.
@@ -35,5 +38,17 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+    @Test
+public void testmain() {
+    PrintStream originalOut = System.out; 
+    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(bos));
+    App.main(null);
+    Assert.assertEquals("hello world", bos.toString());
+    System.setOut(originalOut);
+    dghbejbgekgekj
+	}
+
 }
 //Just to show git demo
